e3863df646751fab9858236520be239dc9aa658b
gi-dor <86302876+gi-dor@users.noreply.github.com>
2024-08-26 11:10:08 +0900

    FEAT : DataGridView , SELECT
    DataGridView Test 및 DB연결을 통한 SELECT 문 조회

---
+++ DataGridViewTest/DataGridViewTest.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Example01
+{
+    partial class DataGridViewTest
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.USER_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.USER_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CUST_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AGY_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.COD_CONT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BRA_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BRA_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSelect = new System.Windows.Forms.Button();
+            this.dataSelectAll = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buuttonSelectDelete = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.USER_ID,
+            this.USER_NAME,
+            this.CUST_ID,
+            this.AGY_ID,
+            this.COD,
+            this.COD_CONT,
+            this.BRA_ID,
+            this.BRA_NAME});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(912, 290);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // USER_ID
+            // 
+            this.USER_ID.HeaderText = "USER_ID";
+            this.USER_ID.Name = "USER_ID";
+            // 
+            // USER_NAME
+            // 
+            this.USER_NAME.HeaderText = "USER_NAME";
+            this.USER_NAME.Name = "USER_NAME";
+            // 
+            // CUST_ID
+            // 
+            this.CUST_ID.HeaderText = "CUST_ID";
+            this.CUST_ID.Name = "CUST_ID";
+            // 
+            // AGY_ID
+            // 
+            this.AGY_ID.HeaderText = "AGY_ID";
+            this.AGY_ID.Name = "AGY_ID";
+            // 
+            // COD
+            // 
+            this.COD.HeaderText = "COD";
+            this.COD.Name = "COD";
+            // 
+            // COD_CONT
+            // 
+            this.COD_CONT.HeaderText = "COD_CONT";
+            this.COD_CONT.Name = "COD_CONT";
+            // 
+            // BRA_ID
+            // 
+            this.BRA_ID.HeaderText = "BRA_ID";
+            this.BRA_ID.Name = "BRA_ID";
+            // 
+            // BRA_NAME
+            // 
+            this.BRA_NAME.HeaderText = "BRA_NAME";
+            this.BRA_NAME.Name = "BRA_NAME";
+            // 
+            // dataSelect
+            // 
+            this.dataSelect.Location = new System.Drawing.Point(3, 90);
+            this.dataSelect.Name = "dataSelect";
+            this.dataSelect.Size = new System.Drawing.Size(161, 39);
+            this.dataSelect.TabIndex = 1;
+            this.dataSelect.Text = "준비된 값 랜덤 ";
+            this.dataSelect.UseVisualStyleBackColor = true;
+            this.dataSelect.Click += new System.EventHandler(this.dataSelect_Click);
+            // 
+            // dataSelectAll
+            // 
+            this.dataSelectAll.Location = new System.Drawing.Point(200, 90);
+            this.dataSelectAll.Name = "dataSelectAll";
+            this.dataSelectAll.Size = new System.Drawing.Size(161, 39);
+            this.dataSelectAll.TabIndex = 2;
+            this.dataSelectAll.Text = "여러건 한번에";
+            this.dataSelectAll.UseVisualStyleBackColor = true;
+            this.dataSelectAll.Click += new System.EventHandler(this.dataSelectAll_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(419, 90);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(161, 39);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "모두 삭제";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buuttonSelectDelete
+            // 
+            this.buuttonSelectDelete.Location = new System.Drawing.Point(628, 90);
+            this.buuttonSelectDelete.Name = "buuttonSelectDelete";
+            this.buuttonSelectDelete.Size = new System.Drawing.Size(161, 39);
+            this.buuttonSelectDelete.TabIndex = 4;
+            this.buuttonSelectDelete.Text = "선택된 행 삭제";
+            this.buuttonSelectDelete.UseVisualStyleBackColor = true;
+            this.buuttonSelectDelete.Click += new System.EventHandler(this.buuttonSelectDelete_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(3, 13);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(161, 38);
+            this.buttonSelect.TabIndex = 5;
+            this.buttonSelect.Text = "조회";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(200, 13);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(161, 38);
+            this.buttonInsert.TabIndex = 6;
+            this.buttonInsert.Text = "1건 삽입 ( 미구현 )";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // DataGridViewTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 691);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.buuttonSelectDelete);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.dataSelectAll);
+            this.Controls.Add(this.dataSelect);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "DataGridViewTest";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn USER_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn USER_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CUST_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AGY_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COD_CONT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BRA_ID;
+        private System.Windows.Forms.Button dataSelect;
+        private System.Windows.Forms.Button dataSelectAll;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buuttonSelectDelete;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BRA_NAME;
+        private System.Windows.Forms.Button buttonInsert;
+    }
+}
+
---
+++ DataGridViewTest/DataGridViewTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.AxHost;
+
+namespace Example01
+{
+    public partial class DataGridViewTest : Form
+    {
+
+        // 데이터베이스 연결
+   
+        public static String uid = "";
+        public static String password = "";  // 비밀번호가 없으므로 빈 문자열
+        public static String database = "";
+        public static String server = "localhost";
+
+        public static String conStr = $"Server={server}; Database={database}; User Id={uid}; Password={password}; Integrated Security=True";
+        SqlConnection conn = new SqlConnection(conStr);
+
+        public DataGridViewTest()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+            conn.Open();
+            MessageBox.Show("DB 연결 되었습니다");
+            } catch(Exception ex)
+            {
+                MessageBox.Show($"DB 연결실패 \r\n Error : {ex.Message}", "DB Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+
+            String sqlQuery = "select * from dbo.gridViewTest order by USER_ID";
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+            // 데이터 읽기
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // DataTable 생성 및 데이터 로드
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+
+            // 데이터그리드뷰에 데이터 바인딩 (컬럼은 디자인에서 정의된 것을 사용)
+            dataGridView1.DataSource = dataTable;
+
+            // 리더와 커맨드를 닫습니다.
+            reader.Close();
+            cmd.Dispose();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            String sqlQuery = "INSERT INTO dbo.gridViewTest (" +
+                "USER_ID , USER_NAME , CUST_ID , AGY_ID , COD , COD_CONT , BRA_ID , BRA_NAME)" +
+                "VALUES " +
+                "('E003134' , '인천 교차지점' , 0297 , 1307 , '' , '' , 100, '인천' )";
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+        }
+        
+
+        private void dataSelect_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            // Next(x) x값보다 작은 랜덤한 양수 반환
+            // 랜덤 돌려서 숫자뽑아서 해당하는 데이터 삽입
+            int randomChart = random.Next(6);
+
+            switch (randomChart)
+            {
+                case 0:
+                    dataGridView1.Rows.Add("red640", "남양주점", "0320", "0089", "BA93", "거래처 일반사용자", "195", "서울");
+                    break;
+                case 1:
+                    dataGridView1.Rows.Add("rf635", "하남점", "0320", "0635", "BA93", "거래처 일반사용자", "075", "하남");
+                    break;
+                case 2:
+                    dataGridView1.Rows.Add("red790", "동부산점", "0320", "0790", "BA93", "거래처 일반사용자", "025", "부산");
+                    break;
+                case 4:
+                    dataGridView1.Rows.Add("rf563", "안산 선부점", "0320", "0563", "BA93", "거래처 일반사용자", "130");
+                    break;
+                case 5:
+                    dataGridView1.Rows.Add("red658", "E 서인천", "0320", "0658", "BA93", "거래처 일반사용자", "100");
+                    break;
+            }
+            return;
+        }
+
+        private void dataSelectAll_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add("red640", "남양주점", "0320", "0089", "BA93", "거래처 일반사용자", "195");
+            dataGridView1.Rows.Add("red790", "동부산점", "0320", "0790", "BA93", "거래처 일반사용자", "025");
+            dataGridView1.Rows.Add("rf635", "하남점", "0320", "0635", "BA93", "거래처 일반사용자", "075");
+            dataGridView1.Rows.Add("rf563", "안산 선부점", "0320", "0563", "BA93", "거래처 일반사용자", "130");
+            dataGridView1.Rows.Add("red658", "E 서인천", "0320", "0658", "BA93", "거래처 일반사용자", "100");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void buuttonSelectDelete_Click(object sender, EventArgs e)
+        {
+            // 선택된 행이 있는지 확인
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int index = dataGridView1.SelectedRows[0].Index;
+
+                dataGridView1.Rows.RemoveAt(index);
+            }
+        }
+
+    }
+}
