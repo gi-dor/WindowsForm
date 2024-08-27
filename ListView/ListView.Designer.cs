a9633e63bf3295d1e636224a73815ee98643fa58
gi-dor <86302876+gi-dor@users.noreply.github.com>
2024-08-27 17:17:45 +0900

    Feat : ListView Test ver1
    gridView
    이미지 첨부 , 이미지 아이콘 첨부 , 사이즈 조정
    클릭이벤트

---
+++ ListView/ListView.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Test
+{
+    partial class ListView
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "colorDialog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel1.Location = new System.Drawing.Point(1248, 446);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(146, 19);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LinkLabel_google";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel2.Location = new System.Drawing.Point(1248, 488);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(137, 19);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "LinkLabel_naver";
+            this.linkLabel2.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.linkLabel3.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel3.Location = new System.Drawing.Point(1248, 529);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(136, 19);
+            this.linkLabel3.TabIndex = 7;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "LinkLabel_daum";
+            this.linkLabel3.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.linkLabel5.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel5.Location = new System.Drawing.Point(1246, 571);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(137, 19);
+            this.linkLabel5.TabIndex = 10;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "ListView 블로그";
+            this.linkLabel5.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(133)))), ((int)(((byte)(104)))));
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuStrip1ToolStripMenuItem,
+            this.contextMenuStrip2ToolStripMenuItem,
+            this.contextMenuStrip3ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(178, 70);
+            // 
+            // contextMenuStrip1ToolStripMenuItem
+            // 
+            this.contextMenuStrip1ToolStripMenuItem.Name = "contextMenuStrip1ToolStripMenuItem";
+            this.contextMenuStrip1ToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.contextMenuStrip1ToolStripMenuItem.Text = "contextMenuStrip1";
+            // 
+            // contextMenuStrip2ToolStripMenuItem
+            // 
+            this.contextMenuStrip2ToolStripMenuItem.Name = "contextMenuStrip2ToolStripMenuItem";
+            this.contextMenuStrip2ToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.contextMenuStrip2ToolStripMenuItem.Text = "contextMenuStrip2";
+            // 
+            // contextMenuStrip3ToolStripMenuItem
+            // 
+            this.contextMenuStrip3ToolStripMenuItem.Name = "contextMenuStrip3ToolStripMenuItem";
+            this.contextMenuStrip3ToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.contextMenuStrip3ToolStripMenuItem.Text = "contextMenuStrip3";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(745, 190);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(346, 541);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(204, 54);
+            this.closeButton.TabIndex = 14;
+            this.closeButton.Text = "closeButton";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 272);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(395, 263);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(464, 272);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(499, 263);
+            this.listView2.TabIndex = 16;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.ItemActivate += new System.EventHandler(this.ListView2_ItemActivate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(487, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "클릭시 유닛 정보 조회";
+            // 
+            // ListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1406, 660);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ListView";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuStrip1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuStrip2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuStrip3ToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ListView/ListView.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class ListView : Form
+    {
+        public ListView()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = products;
+
+            String imagePath = "C:\\Users\\Daun\\Desktop\\image";
+
+
+            /**
+             *  ListView 다루기
+             */
+            listView1.View = View.Details;
+
+            // 컬럼명 추가
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("Lv");
+            listView1.Columns.Add("ATK");
+            listView1.Columns.Add("DEF");
+            listView1.Columns.Add("HP");
+
+            // 보여질 항목을 생성 new ListViewItem(항목명 , 인덱스 순서)
+            ListViewItem item1 = new ListViewItem("무에나", 0);
+            item1.SubItems.Add("90");
+            item1.SubItems.Add("15");
+            item1.SubItems.Add("27");
+            item1.SubItems.Add("220");
+
+            ListViewItem item2 = new ListViewItem("위샤델", 1);
+            item2.SubItems.Add("90");
+            item2.SubItems.Add("19");
+            item2.SubItems.Add("15");
+            item2.SubItems.Add("210");
+
+            ListViewItem item3 = new ListViewItem("특사스", 2);
+            item3.SubItems.Add("90");
+            item3.SubItems.Add("21");
+            item3.SubItems.Add("19");
+            item3.SubItems.Add("280");
+
+            ListViewItem item4 = new ListViewItem("총웨", 3);
+            item4.SubItems.Add("90");
+            item4.SubItems.Add("15");
+            item4.SubItems.Add("30");
+            item4.SubItems.Add("300");
+
+
+            // ListView 이미지
+            ImageList imageListSmall = new ImageList();
+
+            imageListSmall.Images.Add(Image.FromFile(imagePath + "\\무에나.jpg"));  // 무에나 이미지 추가
+            imageListSmall.Images.Add(Image.FromFile(imagePath + "\\위샤델.jpg"));  // 위샤델 이미지 추가
+            imageListSmall.Images.Add(Image.FromFile(imagePath + "\\특사스.jpg"));  // 특사스 이미지 추가
+            imageListSmall.Images.Add(Image.FromFile(imagePath + "\\총웨.jpg"));  // 총웨 이미지 추가
+
+  
+            // ImageList 크기 조절
+            imageListSmall.ImageSize = new Size(50, 50); // Small Icon 크기 설정
+
+
+            // ListView에 ImageList 연결
+            listView1.SmallImageList = imageListSmall;
+
+
+            // 데이터 삽입
+            listView1.Items.Add(item1);
+            listView1.Items.Add(item2);
+            listView1.Items.Add(item3);
+            listView1.Items.Add(item4);
+
+            // 컬럼 너비 자동 조정
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+
+
+            // **********************************************************************************************************   //
+
+            /**
+          *  ListView2 다루기
+          */
+          
+            listView2.View = View.LargeIcon;
+
+            // 컬럼명 추가
+            listView2.Columns.Add("이름");
+            listView2.Columns.Add("ATK");
+            listView2.Columns.Add("DEF");
+            listView2.Columns.Add("ASPD");
+            listView2.Columns.Add("HP");
+
+
+            // ListView 이미지
+            ImageList imageListLarge = new ImageList();
+
+            imageListLarge.Images.Add(Image.FromFile(imagePath + "\\무에나.jpg"));  // 무에나 이미지 추가
+            imageListLarge.Images.Add(Image.FromFile(imagePath + "\\위샤델.jpg"));  // 위샤델 이미지 추가
+            imageListLarge.Images.Add(Image.FromFile(imagePath + "\\특사스.jpg"));  // 특사스 이미지 추가
+            imageListLarge.Images.Add(Image.FromFile(imagePath + "\\총웨.jpg"));  // 총웨 이미지 추가
+
+
+            // 이미지 사이즈 조정
+            imageListLarge.ImageSize = new Size(180, 100); // Large Icon 크기 설정
+
+            // ListView2에 ImageList 연결
+            listView2.LargeImageList = imageListLarge;
+
+
+
+            // 컬럼 내용 추가 new ListViewItem(항목명 , 인덱스 순서)
+            ListViewItem item21 = new ListViewItem("무에나", 0);
+            item21.SubItems.Add("15");
+            item21.SubItems.Add("27");
+            item21.SubItems.Add("180");
+            item21.SubItems.Add("220");
+
+            ListViewItem item22 = new ListViewItem("위샤델", 1);
+            item22.SubItems.Add("19");
+            item22.SubItems.Add("15");
+            item22.SubItems.Add("190");
+            item22.SubItems.Add("210");
+
+            ListViewItem item23 = new ListViewItem("특사스", 2);
+            item23.SubItems.Add("21");
+            item23.SubItems.Add("19");
+            item23.SubItems.Add("193");
+            item23.SubItems.Add("280");
+
+            ListViewItem item24 = new ListViewItem("총웨", 3);
+            item24.SubItems.Add("15");
+            item24.SubItems.Add("30");
+            item24.SubItems.Add("185");
+            item24.SubItems.Add("300");
+
+          
+
+            // 데이터 삽입
+            listView2.Items.Add(item21);
+            listView2.Items.Add(item22);
+            listView2.Items.Add(item23);
+            listView2.Items.Add(item24);
+
+
+
+         
+
+
+            // 컬럼 너비 자동 조정
+            listView2.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView2.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+        }
+
+       // ListView2 아이콘 클릭시 해당 정보 보이는 이벤트
+        private void ListView2_ItemActivate(object sender, EventArgs e)
+        {
+            if(listView2.SelectedItems.Count > 0 )
+            {
+                ListViewItem selectItem = listView2.SelectedItems[0];
+                String name = selectItem.Text;
+                String atk = selectItem.SubItems[1].Text;
+                String def = selectItem.SubItems[2].Text;
+                String aspd = selectItem.SubItems[3].Text;
+                String hp = selectItem.SubItems[4].Text;
+
+                MessageBox.Show($"이름  : {name}\nATK  : {atk}\nDEF  : {def}\nHP   : {hp}", "캐릭터 정보");
+            }
+        }
+
+
+
+        // colorDialog 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+
+            color.ShowHelp = true;
+
+            color.Color = this.BackColor;
+
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = color.Color;
+            }
+        }
+
+
+
+        // 하이퍼링크 버튼
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://google.com");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://naver.com");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://daum.net");
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://oioirang.tistory.com/148");
+        }
+
+     
+        
+            
+        // form 종료
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+      
+        
+        
+
+
+        /******************************************************************************************/
+        // dataGridView
+       
+       public class Product
+       {
+           public String id { get; set; }
+
+           public String name { get; set; }
+
+           public int ATK { get; set; }
+
+           public int DEF { get; set; }
+
+           public int ASPD { get; set; }
+
+           public int HP { get; set; }
+
+           public int MP { get; set; }
+
+        }
+
+
+        List<Product> products = new List<Product>
+
+       {
+           new Product { id = "0001" , name = " 무에나 " , ATK = 15 , DEF = 27 , ASPD = 10 , HP = 210 , MP = 180 },
+           new Product { id = "0002" ,  name = " 위샤델 " , ATK = 19 , DEF = 15 , ASPD = 20 , HP = 210 , MP = 150 } ,
+           new Product { id = "0003" , name = " 특사스 " , ATK = 21 , DEF = 19 , ASPD = 10 , HP = 280 , MP = 200} , 
+           new Product { id = "0004" ,  name = " 총웨 " , ATK = 15 , DEF = 30 , ASPD = 15 , HP = 300 , MP = 100}
+       };
+
+    }
+}
